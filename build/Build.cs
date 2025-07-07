e12a54dbd4249e2604a273eeed634a6924e506b5
kaitpw <kaitpw@gmail.com>
2025-07-08 00:33:27 +0900

    beginning to make the github actions flow

--- build/Build.cs
+++ build/Build.cs
@@ -1,11 +1,25 @@
 using System;
 using System.IO;
 using Nuke.Common;
+using Nuke.Common.CI.GitHubActions;
 using Nuke.Common.IO;
 using ricaun.Nuke;
 using ricaun.Nuke.Components;
 using Serilog;
 
+// This allows generation of a GitHub Actions workflow file, however env vars needto be added manually
+// env:
+//   GitHubToken: ${{ secrets.GITHUB_TOKEN }}
+//   NugetApiUrl: ${{ secrets.NUGET_API_URL }}
+//   NugetApiKey: ${{ secrets.NUGET_API_KEY }}
+//   SignFile: ${{ secrets.SIGN_FILE }}
+//   SignPassword: ${{ secrets.SIGN_PASSWORD }}
+[GitHubActions(
+    "continuous",
+    GitHubActionsImage.WindowsLatest,
+    On = new[] { GitHubActionsTrigger.Push },
+    InvokedTargets = new[] { nameof(Build) }
+)]
 class Build : NukeBuild, IPublishRevit
 {
     //string IHazMainProject.MainName => "PE_Tools";
