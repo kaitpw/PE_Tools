ed2b603ff633e537a4a6c22ed804b17ad57b942d
kaitpw <kaitpw@gmail.com>
2025-08-30 10:34:12 +0900

    add default framework and version for Cursor LSP. Also changes the way the default Debug and Release configs work (now it picks up the default framwork and version rather than defaulting to R26)

--- Library/PeGlobal/GlobalUsing.cs
+++ Library/PeGlobal/GlobalUsing.cs
@@ -1,9 +1,12 @@
 ﻿// these global using statements will appear in every code file in the project
 
 global using System;
+global using System.Collections.Generic;
 global using System.Diagnostics;
 global using System.IO;
+global using System.Linq;
 global using System.Reflection;
+global using System.Threading.Tasks;
 global using Autodesk.Revit.Attributes;
 global using Autodesk.Revit.DB;
 global using Autodesk.Revit.UI;
