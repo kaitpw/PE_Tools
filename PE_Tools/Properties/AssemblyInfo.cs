8feb8be3d415ce74e86838e62b40be0d3ccf1d88
kaitpw <kaitpw@gmail.com>
2025-11-21 00:11:18 +0900

    update version

--- PE_Tools/Properties/AssemblyInfo.cs
+++ PE_Tools/Properties/AssemblyInfo.cs
@@ -14,4 +14,4 @@
 // The following GUID is for the ID of the typeRevit.Lib if this project is exposed to COM
 [assembly: Guid("04b918ec-6956-4fd9-ac34-1018929b7763")]
 
-[assembly: AssemblyVersion("1.9.0")]
+[assembly: AssemblyVersion("1.10.0")]
